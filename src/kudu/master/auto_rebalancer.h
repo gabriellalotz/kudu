d08be701c60f5f134bcda53a5221201cea29a8c0
宋家成 <songjiacheng@thinkingdata.cn>
2024-02-26 17:29:21 +0800

    Remove the replace flag if the move fails in auto rebalancing.
    If a move fails because of some reasons, like high workload, the
    auto rebalancer will remove the ReplicaMove from the vector and
    just skip it to next, leaving the replace flag still true. And then
    the master will add a new peer to the tablet, trying to replace the
    original replica. But if the replica is a leader, it will never be
    replaced since no one will send LeaderStepDown request to it.
    
    So remove the replace flag of the original replica before removing
    the move, rollback the modification if it fails.
    
    Change-Id: I99dafa654878b9d6d8162d84500913ae0655692b

--- src/kudu/master/auto_rebalancer.h
+++ src/kudu/master/auto_rebalancer.h
@@ -135,6 +135,9 @@ class AutoRebalancerTask {
   // to have them moved in order to rebalance the cluster.
   // Returns a non-OK status if the replica or the replica's tserver
   // cannot be found, or the request to move the replica cannot be completed.
+  //
+  // Some information that is used to removing the replace flag if the moves
+  // fail lately will be added to the ReplicaMoves in this method.
   Status ExecuteMoves(
       const std::vector<rebalance::Rebalancer::ReplicaMove>& replica_moves);
 
