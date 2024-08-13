16fd2eb82d4c40c2a165617d34bd5ae7f7293f33
Gabriella Lotz <gabriellalotz@admins-MacBook-Pro.local>
2024-08-13 17:15:33 +0200

    wip: Add TablesResponse model and update get_tables endpoint

--- python/rest_api/main.py
+++ python/rest_api/main.py
@@ -7,6 +7,7 @@ from kudu.client import Partitioning
 from pydantic import BaseModel
 from typing import List, Union
 from typing_extensions import Annotated
+from fastapi.exceptions import HTTPException
 
 # Define the schemas for the API.
 
@@ -37,6 +38,9 @@ class RangePartition(BaseModel):
     upper_bound: None
     lower_bound_type: str
     upper_bound_type: str
+    
+class TablesResponse(BaseModel):
+    tables: List[str]
 
 
 app = FastAPI(root_path="/api/v1")
@@ -51,7 +55,7 @@ def read_root():
 
 
 @app.get("/tables")
-def get_tables():
+def get_tables() -> TablesResponse:
     tables = client.list_tables()
     return {"tables": tables}
 
@@ -59,24 +63,16 @@ def get_tables():
 
 
 @app.get("/tables/{table}")
-def get_table(table: str, details: str = Query("schema", description="schema or full", enum=["schema", "full"])):
+def get_table(table: str):
     table = client.table(table)
-    if details == "schema":
-        table_dict = schema_to_dict(table.schema)
-    else:
-        table_dict = {"columns": []}
-        for i in range(len(table.schema)):
-            column = table.schema.at(i)
-            table_dict["columns"].append({"name": column.name, "type": str(
-                column.type), "nullable": column.nullable, "primary_key": column.name in table.schema.primary_keys()})
-        # TODO: add partitions, tablets, etc.
+    table_dict = schema_to_dict(table)
     return {table.name: table_dict}
 
 # POST api/v1/tables - create new table
 # TODO: try out different examples, query parameters could be in different formats
 
 
-@app.post("/tables")
+@app.post("/tables", status_code=201)
 def post_table(name: str, table_schema: Schema, hash_partitioning: HashPartition, range_partitioning: RangePartition):
     # Define a schema for a new table.
     builder = kudu.schema_builder()
@@ -90,40 +86,47 @@ def post_table(name: str, table_schema: Schema, hash_partitioning: HashPartition
     schema = builder.build()
     # Define the partitioning schema.
     partitioning = Partitioning()
-    if hash_partitioning:
-        partitioning.add_hash_partitions(
-            column_names=hash_partitioning.column_names, num_buckets=hash_partitioning.num_buckets)
-    if range_partitioning:
-        partitioning.add_range_partition(lower_bound=range_partitioning.lower_bound, upper_bound=range_partitioning.upper_bound,
-                                         lower_bound_type=range_partitioning.lower_bound_type, upper_bound_type=range_partitioning.upper_bound_type)
-    # TODO: Throw error if table already exists.
+    # TODO: put default values in schemas
+    # if hash_partitioning:
+    #     partitioning.add_hash_partitions(
+    #         column_names=hash_partitioning.column_names, num_buckets=hash_partitioning.num_buckets)
+    # if range_partitioning:
+    #     partitioning.add_range_partition(lower_bound=range_partitioning.lower_bound, upper_bound=range_partitioning.upper_bound,
+    #                                      lower_bound_type=range_partitioning.lower_bound_type, upper_bound_type=range_partitioning.upper_bound_type)
+    # Throw error if table already exists.
+    if client.table_exists(name):
+        raise HTTPException(status_code=409, detail="Table already exists.")
     # Create a new table.
     client.create_table(name, schema, partitioning)
     table = client.table(name)
-    # TODO: talk to the team about the return value
-    return {table.name: schema_to_dict(table.schema)}
+    # Return the table
+    return {table.name: schema_to_dict(table)}
 
 # DELETE api/v1/tables/{table} - drop table
 
 
-@app.delete("/tables/{table}")
+@app.delete("/tables/{table}", status_code=204)
 def delete_table(table: str):
     client.delete_table(table)
-    # should return 204 with empty body
+    return
 
 # PUT api/v1/tables/{table} - update table
 
 
 @app.put("/tables/{table}")
-def put_table(table: str, table_schema: Schema):
+def put_table(table: str, new_table_name: Union[str, None] = None, table_schema: Schema = None):
     table = client.table(table)
+    if new_table_name:
+        alter_table = client.table(new_table_name)
+        if alter_table:
+            raise HTTPException(status_code=409, detail="Table already exists.")
+        # rename table
     if len(table.schema) != len(table_schema.columns):
         # add new column
         pass
     # partition check
-    return {"table": table}
+    return {"table": schema_to_dict(table)}
     
-
 # GET api/v1/tables/{table}/partitions - list partitions
 # GET api/v1/tables/{table}/tablets - list tablets
 
--- python/rest_api/utils.py
+++ python/rest_api/utils.py
@@ -1,8 +1,16 @@
-def schema_to_dict(schema):
-    primary_key = schema.primary_keys()
-    columns = []
-    for i in range(len(schema)):
-        columns.append(schema.at(i).name + " " + str(schema.at(i).type))
-        if schema.at(i).name in primary_key:
-            columns[-1] += " PRIMARY KEY"
-    return columns
+# def schema_to_dict(schema):
+#     primary_key = schema.primary_keys()
+#     columns = []
+#     for i in range(len(schema)):
+#         columns.append(schema.at(i).name + " " + str(schema.at(i).type))
+#         if schema.at(i).name in primary_key:
+#             columns[-1] += " PRIMARY KEY"
+#     return columns
+
+def schema_to_dict(table):
+    table_dict = {"columns": []}
+    for i in range(len(table.schema)):
+        column = table.schema.at(i)
+        table_dict["columns"].append({"name": column.name, "type": str(
+            column.type), "nullable": column.nullable, "primary_key": column.name in table.schema.primary_keys()})
+    return table_dict
