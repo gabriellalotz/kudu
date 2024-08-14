6f92271ceb601d73b4ab3472cea5d903e7ffeac6
Gabriella Lotz <lotzgabriella@gmail.com>
2024-08-14 16:20:06 +0200

    implement PUT request to table rename and column operations

--- python/rest_api/utils.py
+++ python/rest_api/utils.py
@@ -11,6 +11,16 @@ def schema_to_dict(table):
     table_dict = {"columns": []}
     for i in range(len(table.schema)):
         column = table.schema.at(i)
-        table_dict["columns"].append({"name": column.name, "type": str(
-            column.type), "nullable": column.nullable, "primary_key": column.name in table.schema.primary_keys()})
+        type = str(column.type)
+        type = type[type.index("(")+1:type.index(")")]
+        table_dict["columns"].append({"name": column.name, "type": type, "nullable": column.nullable,
+                                     "primary_key": column.name in table.schema.primary_keys()})
     return table_dict
+
+
+def column_names(schema):
+    column_names = []
+    for i in range(len(schema)):
+        column = schema.at(i)
+        column_names.append(column.name)
+    return column_names
