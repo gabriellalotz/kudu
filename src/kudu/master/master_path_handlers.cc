f437a209a0dbf557f33c6535d9c193b408ae2bd4
gabriellalotz <lotzgabriella@gmail.com>
2024-09-18 11:55:55 +0200

    [WIP] KUDU-3608: REST API for Metadata Management
    This commit introduces a REST API integrated directly into the Kudu
    master web server to simplify metadata access and management. The API
    operates under the /api path and focuses exclusively on
    metadata-related operations. The following endpoints have been added:
    
    - GET /api/v1/tables - Lists all tables by name and ID.
    - POST /api/v1/tables - Creates a new table.
    - GET /api/v1/tables/<table_id> - Retrieves table details.
    - PUT /api/v1/tables/<table_id> - Updates table metadata.
    - DELETE /api/v1/tables/<table_id> - Deletes a table.
    
    HTTP Status Codes: Introduced additional HTTP status codes like 201
    Created and 204 No Content for operations such as creating and deleting
    tables.
    Enhanced the web server's content-length check to include the PUT
    method, allowing it to handle errors similar to POST.
    
    Limitations: No Security Measures Yet. This implementation does not
    currently include authentication or authorization.
    Deleting, altering, or retrieving a table object requires the table's
    ID. Operations using table names are not yet supported.
    
    Testing: A new test file called rest_catalog-test.cc has been created to
    validate these endpoints using an internal mini-cluster setup.
    
    Usage:
    When running Kudu, the endpoints are accessible via the /api/v1/ path
    on the master web server. For POST and PUT endpoints, appropriate
    data must be provided with the HTTP request (e.g., via curl).
    
    Change-Id: I67f964c4f950edfde31772cafd5c3ed5d6b87413

--- src/kudu/master/master_path_handlers.cc
+++ src/kudu/master/master_path_handlers.cc
@@ -32,7 +32,9 @@
 #include <utility>
 #include <vector>
 
+#include "google/protobuf/util/json_util.h"
 #include <glog/logging.h>
+#include <google/protobuf/stubs/status.h>
 
 #include "kudu/common/common.pb.h"
 #include "kudu/common/partition.h"
@@ -77,17 +79,30 @@
 #include "kudu/util/url-coding.h"
 #include "kudu/util/web_callback_registry.h"
 
+#define RETURN_JSON_ERROR(jw, error_msg, status_code, error_code) \
+  {                                                               \
+    (jw).StartObject();                                           \
+    (jw).String("error");                                         \
+    (jw).String(error_msg);                                       \
+    (jw).EndObject();                                             \
+    (status_code) = (error_code);                                 \
+    return;                                                       \
+  }
+
 using kudu::consensus::ConsensusStatePB;
 using kudu::consensus::RaftPeerPB;
 using kudu::security::DataFormat;
 using std::array;
 using std::map;
+using std::optional;
 using std::ostringstream;
 using std::pair;
 using std::shared_ptr;
 using std::string;
 using std::vector;
 using strings::Substitute;
+using google::protobuf::util::JsonStringToMessage;
+using google::protobuf::util::JsonParseOptions;
 
 namespace kudu {
 namespace master {
@@ -110,6 +125,226 @@ struct TServerStateInfo {
 } // anonymous namespace
 
 MasterPathHandlers::~MasterPathHandlers() {
+}
+
+void MasterPathHandlers::HandleApiTableEndpoint(const Webserver::WebRequest& req,
+                                             Webserver::PrerenderedWebResponse* resp) {
+  string table_id = req.path_params.at("table_id");
+  ostringstream* output = &resp->output;
+  JsonWriter jw(output, JsonWriter::COMPACT);
+
+  if (table_id.length() != 32) {
+    RETURN_JSON_ERROR(jw, "Invalid table ID", resp->status_code, HttpStatusCode::BadRequest);
+  }
+  CatalogManager::ScopedLeaderSharedLock l(master_->catalog_manager());
+  scoped_refptr<TableInfo> table;
+  Status status = master_->catalog_manager()->GetTableInfo(table_id, &table);
+
+  if (!status.ok()) {
+    RETURN_JSON_ERROR(jw, status.ToString(), resp->status_code, HttpStatusCode::ServiceUnavailable);
+  }
+
+  if (!table) {
+    RETURN_JSON_ERROR(jw, "Table not found", resp->status_code, HttpStatusCode::NotFound);
+  }
+
+  if (req.request_method == "GET") {
+    HandleGetTable(output, req, &resp->status_code);
+  } else if (req.request_method == "PUT") {
+    HandlePutTable(output, req, &resp->status_code);
+  } else if (req.request_method == "DELETE") {
+    HandleDeleteTable(output, req, &resp->status_code);
+  } else {
+    RETURN_JSON_ERROR(
+        jw, "Method not allowed", resp->status_code, HttpStatusCode::MethodNotAllowed);
+  }
+}
+
+void MasterPathHandlers::HandleApiTablesEndpoint(const Webserver::WebRequest& req,
+                                                 Webserver::PrerenderedWebResponse* resp) {
+  ostringstream* output = &resp->output;
+
+  if (req.request_method == "GET") {
+    CatalogManager::ScopedLeaderSharedLock l(master_->catalog_manager());
+    HandleGetTables(output, &resp->status_code);
+  } else if (req.request_method == "POST") {
+    CatalogManager::ScopedLeaderSharedLock l(master_->catalog_manager());
+    HandlePostTables(output, req, &resp->status_code);
+  } else {
+    JsonWriter jw(output, JsonWriter::COMPACT);
+    RETURN_JSON_ERROR(
+        jw, "Method not allowed", resp->status_code, HttpStatusCode::MethodNotAllowed);
+  }
+}
+
+void MasterPathHandlers::HandleGetTables(std::ostringstream* output, HttpStatusCode* status_code) {
+  ListTablesRequestPB request;
+  ListTablesResponsePB response;
+  // TODO(gabriellalotz): get username
+  std::optional<std::string> user = "root";
+  Status status = master_->catalog_manager()->ListTables(&request, &response, user);
+  JsonWriter jw(output, JsonWriter::COMPACT);
+
+  if (!status.ok()) {
+    RETURN_JSON_ERROR(jw, status.ToString(), *status_code, HttpStatusCode::ServiceUnavailable);
+  }
+  jw.StartObject();
+  jw.String("tables");
+  jw.StartArray();
+
+  for (const auto& table : response.tables()) {
+    jw.StartObject();
+    jw.String("table_id");
+    jw.String(table.id());
+    jw.String("table_name");
+    jw.String(table.name());
+    jw.EndObject();
+  }
+  jw.EndArray();
+  jw.EndObject();
+  *status_code = HttpStatusCode::Ok;
+}
+
+void MasterPathHandlers::HandlePostTables(std::ostringstream* output,
+                                          const Webserver::WebRequest& req,
+                                          HttpStatusCode* status_code) {
+  CreateTableRequestPB request;
+  CreateTableResponsePB response;
+
+  const string& json_str = req.post_data;
+  JsonParseOptions opts;
+  opts.case_insensitive_enum_parsing = true;
+  google::protobuf::util::Status google_status = JsonStringToMessage(json_str, &request, opts);
+
+  if (!google_status.ok()) {
+    JsonWriter jw(output, JsonWriter::COMPACT);
+    RETURN_JSON_ERROR(jw,
+                      Substitute("JSON table object is not correct: $0", json_str),
+                      *status_code,
+                      HttpStatusCode::BadRequest);
+  }
+  Status status = master_->catalog_manager()->CreateTable(&request, &response, nullptr);
+
+  if (status.ok()) {
+    PrintTableObject(output, response.table_id());
+    *status_code = HttpStatusCode::Created;
+  } else {
+    JsonWriter jw(output, JsonWriter::COMPACT);
+    RETURN_JSON_ERROR(jw, status.ToString(), *status_code, HttpStatusCode::ServiceUnavailable);
+  }
+
+}
+
+void MasterPathHandlers::HandleGetTable(std::ostringstream* output,
+                                        const Webserver::WebRequest& req,
+                                        HttpStatusCode* status_code) {
+  string table_id = req.path_params.at("table_id");
+  PrintTableObject(output, table_id);
+  *status_code = HttpStatusCode::Ok;
+}
+
+void MasterPathHandlers::HandlePutTable(std::ostringstream* output,
+                                        const Webserver::WebRequest& req,
+                                        HttpStatusCode* status_code) {
+  string table_id = req.path_params.at("table_id");
+  AlterTableRequestPB request;
+  AlterTableResponsePB response;
+  request.mutable_table()->set_table_id(table_id);
+
+  const string& json_str = req.post_data;
+  JsonParseOptions opts;
+  opts.case_insensitive_enum_parsing = true;
+  google::protobuf::util::Status google_status = JsonStringToMessage(json_str, &request, opts);
+
+  if (!google_status.ok()) {
+    JsonWriter jw(output, JsonWriter::COMPACT);
+    RETURN_JSON_ERROR(jw,
+                      Substitute("JSON table object is not correct: $0", json_str),
+                      *status_code,
+                      HttpStatusCode::BadRequest);
+  }
+
+  Status status = master_->catalog_manager()->AlterTableRpc(request, &response, nullptr);
+
+  if (!status.ok()) {
+    JsonWriter jw(output, JsonWriter::COMPACT);
+    RETURN_JSON_ERROR(jw, status.ToString(), *status_code, HttpStatusCode::ServiceUnavailable);
+  }
+
+  IsAlterTableDoneRequestPB check_req;
+  IsAlterTableDoneResponsePB check_resp;
+  check_req.mutable_table()->set_table_id(table_id);
+  MonoTime deadline = MonoTime::Now() + MonoDelta::FromSeconds(60);
+  const optional<string> user = "root";
+
+  while (MonoTime::Now() < deadline) {
+    status = master_->catalog_manager()->IsAlterTableDone(&check_req, &check_resp, user);
+
+    if (!status.ok()) {
+      JsonWriter jw(output, JsonWriter::COMPACT);
+      RETURN_JSON_ERROR(jw, status.ToString(), *status_code, HttpStatusCode::ServiceUnavailable);
+    }
+
+    if (check_resp.has_error()) {
+      JsonWriter jw(output, JsonWriter::COMPACT);
+      RETURN_JSON_ERROR(jw,
+                        check_resp.error().ShortDebugString(),
+                        *status_code,
+                        HttpStatusCode::ServiceUnavailable);
+    }
+
+    if (check_resp.done()) {
+      PrintTableObject(output, table_id);
+      *status_code = HttpStatusCode::Ok;
+      return;
+    }
+    SleepFor(MonoDelta::FromMilliseconds(200));
+  }
+  JsonWriter jw(output, JsonWriter::COMPACT);
+  RETURN_JSON_ERROR(jw, "Alter table timed out", *status_code, HttpStatusCode::ServiceUnavailable);
+}
+
+void MasterPathHandlers::HandleDeleteTable(std::ostringstream* output,
+                                           const Webserver::WebRequest& req,
+                                           HttpStatusCode* status_code) {
+  string table_id = req.path_params.at("table_id");
+  DeleteTableRequestPB request;
+  DeleteTableResponsePB response;
+  request.mutable_table()->set_table_id(table_id);
+  JsonWriter jw(output, JsonWriter::COMPACT);
+  Status status = master_->catalog_manager()->DeleteTableRpc(request, &response, nullptr);
+
+  if (status.ok()) {
+    *status_code = HttpStatusCode::NoContent;
+  } else {
+    RETURN_JSON_ERROR(jw, status.ToString(), *status_code, HttpStatusCode::ServiceUnavailable);
+  }
+}
+
+void MasterPathHandlers::PrintTableObject(std::ostringstream* output, const string& table_id) {
+  scoped_refptr<TableInfo> table;
+  Status status = master_->catalog_manager()->GetTableInfo(table_id, &table);
+  JsonWriter jw(output, JsonWriter::COMPACT);
+
+  jw.StartObject();
+  {
+    TableMetadataLock l(table.get(), LockMode::READ);
+    jw.String("name");
+    jw.String(l.data().name());
+    jw.String("id");
+    jw.String(table_id);
+    jw.String("schema");
+    jw.Protobuf(l.data().pb.schema());
+    jw.String("partition_schema");
+    jw.Protobuf(l.data().pb.partition_schema());
+    jw.String("owner");
+    jw.String(l.data().owner());
+    jw.String("comment");
+    jw.String(l.data().comment());
+    jw.String("extra_config");
+    jw.Protobuf(l.data().pb.extra_config());
+  }
+  jw.EndObject();
 }
 
 void MasterPathHandlers::HandleTabletServers(const Webserver::WebRequest& /*req*/,
@@ -874,6 +1109,18 @@ void MasterPathHandlers::HandleDumpEntities(const Webserver::WebRequest& /*req*/
 
 Status MasterPathHandlers::Register(Webserver* server) {
   constexpr const bool is_on_nav_bar = true;
+  server->RegisterPrerenderedPathHandler(
+      "/api/v1/tables/<table_id>", "",
+      [this](const Webserver::WebRequest& req, Webserver::PrerenderedWebResponse* resp) {
+        this->HandleApiTableEndpoint(req, resp);
+      },
+      StyleMode::JSON, false);
+  server->RegisterPrerenderedPathHandler(
+      "/api/v1/tables", "",
+      [this](const Webserver::WebRequest& req, Webserver::PrerenderedWebResponse* resp) {
+        this->HandleApiTablesEndpoint(req, resp);
+      },
+      StyleMode::JSON, false);
   server->RegisterPathHandler(
       "/tablet-servers", "Tablet Servers",
       [this](const Webserver::WebRequest& req, Webserver::WebResponse* resp) {
